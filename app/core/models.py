650847ece3de0b0ff0d03faa9fbc0f85c2083652
Idan Narotzki <idann@ownbackup.com>
2021-06-06 00:30:46 +0300

    updated admin to support custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -6,10 +6,9 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,\
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        """ Creates and saves a new user"""
+        """Creates and saves a new User"""
         if not email:
-            raise ValueError('User must have email address')
-
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
